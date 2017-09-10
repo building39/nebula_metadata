c9c9c6e95d16a826c8c33c187838613ee0ca0c0a
mmartin <mmartin4242@gmail.com>
2017-09-10 17:02:02 -0400

    Bump version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule NebulaMetadata.Mixfile do
 
   def project do
     [app: :nebula_metadata,
-     version: "0.1.0",
+     version: "0.2.3",
      elixir: "~> 1.5",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
