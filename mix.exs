a284886b292d736ec75dc00a7d79e3f0e7dddb1e
mmartin <mmartin4242@gmail.com>
2018-01-06 16:13:49 -0600

    Require elixir 1.6 or greater

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule NebulaMetadata.Mixfile do
   def project do
     [app: :nebula_metadata,
      version: "0.2.5",
-     elixir: "~> 1.5",
+     elixir: "~> 1.6",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps()]
