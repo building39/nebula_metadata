e5697e7b80e361c6411fdb1166afe7371abd0e1d
mmartin <mmartin4242@gmail.com>
2018-02-02 09:48:58 -0500

    Remove exrm dependency

--- mix.exs
+++ mix.exs
@@ -42,8 +42,7 @@ defmodule NebulaMetadata.Mixfile do
     [{:logger_file_backend, "~> 0.0"},
      {:riak, "~> 1.0"},
      {:poison, "~> 2.2"},
-     {:memcache_client, git: "https://github.com/tsharju/memcache_client.git", tag: "master"},
-     {:exrm, "~> 1.0"}
+     {:memcache_client, git: "https://github.com/tsharju/memcache_client.git", tag: "master"}
     ]
   end
 end
